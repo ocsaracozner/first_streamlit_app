e25ba0d1e447906c74cfdc86f0792ff90cfdec5a
ocsaracozner <106470109+ocsaracozner@users.noreply.github.com>
2022-11-09 15:29:57 +0800

    Update streamlit_app.py
    Added header
    Added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 
 
 streamlit.title('My first streamlit app!')
+streamlit.header("A new beginning.")
+streamlit.text("Streamlit text example.")
+streamlit.text("Streamlit text another example.")
